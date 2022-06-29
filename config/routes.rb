9c79c3c4e62675dc9f04cd350448a351637672b0
keerthana-shanmugam <keerthanas089@gmail.com>
2022-06-29 13:52:29 +0530

    fixed-route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root "todos/index"
   get 'beauty_apps/index'
   match 'a', :to => 'beauty_apps#index', :via => :get
 
   match 'users/new_file', :to => 'users#new_file', :via => :get
   match 'users/create', :to => 'users#create', :via => :post
 
-  match 'todos/index', :to => 'todos#index', :via => :get
+  
   match 'todos/create', :to => 'todos#create', :via => :post
   get '/delete/:id', :to => 'todos#delete', :via => :delete
   match 'todos/update/:id', to: 'todos#update', via:  :get
